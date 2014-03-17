9ec00381b336281ddd11d6c98ae7a2e0dc967b19
Andres Velasquez <andres@sirius.utp.edu.co>
2014-03-17 08:36:49 -0500

    add TestHeapInsertSort

---
+++ testHeapInsertSort.sh
@@ -0,0 +1,14 @@
+
+
+echo "Testing heapinsert sort"
+
+./trabajoFinal --heapinsertsort "100000.rand" 
+./trabajoFinal --heapinsertsort "200000.rand" 
+./trabajoFinal --heapinsertsort "300000.rand" 
+./trabajoFinal --heapinsertsort "400000.rand" 
+./trabajoFinal --heapinsertsort "500000.rand" 
+./trabajoFinal --heapinsertsort "600000.rand" 
+./trabajoFinal --heapinsertsort "700000.rand" 
+./trabajoFinal --heapinsertsort "800000.rand" 
+./trabajoFinal --heapinsertsort "900000.rand" 
+./trabajoFinal --heapinsertsort "1000000.rand" 
